381b2b3833e21d7d04327d9dada3276eda9eb3e5
Joe Farrell <disastrousgamemaster@gmail.com>
2019-10-28 19:39:48 -0400

    Other Python mistake

--- src/api/MarketService.py
+++ src/api/MarketService.py
@@ -2,17 +2,19 @@ import json
 import requests
 from src.models.Stock import Stock
 
-API_TOKEN = ""
-API_URL = "https://sandbox.tradier.com/v1/"
+class MarketService:
 
-HEADERS = {"Content-Type": "application/json", "Authorization": "Bearer {0}".format(API_TOKEN)}
+    API_TOKEN = ""
+    API_URL = "https://sandbox.tradier.com/v1/"
 
-def getStock(symbol: str) -> Stock:
-    response = requests.get(API_URL + 'markets/quotes',
-    params={'symbols': symbol, 'greeks': 'false'},
-    headers={'Authorization': API_TOKEN, 'Accept': 'application/json'}
-    
-    stock = Stock()
-    json.dumps(stock)
-    return stock
+    HEADERS = {"Content-Type": "application/json", "Authorization": "Bearer {0}".format(API_TOKEN)}
+
+    def getStock(self, symbol: str) -> Stock:
+        response = requests.get(API_URL + 'markets/quotes',
+        params={'symbols': symbol, 'greeks': 'false'},
+        headers={'Authorization': API_TOKEN, 'Accept': 'application/json'}
+        
+        stock = Stock()
+        json.dumps(stock)
+        return stock
 
