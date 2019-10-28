a4b19d0f4556f36b200f52aeba9972f13947a924
Joe Farrell <disastrousgamemaster@gmail.com>
2019-10-28 19:37:47 -0400

    Python mistake

--- src/models/Stock.py
+++ src/models/Stock.py
@@ -1,12 +1,12 @@
 class Stock:
-    def __init__(open: float, close: float, high: float, low: float, average_volume: float, dividend_yield: float, type: string, last: float, symbol: string, prevclose: float):
-        this.open = open
-        this.close = close
-        this.high = high
-        this.low = low
-        this.average_volume = average_volume
-        this.dividend_yield = dividend_yield
-        this.type = type
-        this.last = last
-        this.symbol = symbol
-        this.prevclose = prevclose
+    def __init__(self, open: float, close: float, high: float, low: float, average_volume: float, dividend_yield: float, type: string, last: float, symbol: string, prevclose: float):
+        self.open = open
+        self.close = close
+        self.high = high
+        self.low = low
+        self.average_volume = average_volume
+        self.dividend_yield = dividend_yield
+        self.type = type
+        self.last = last
+        self.symbol = symbol
+        self.prevclose = prevclose
