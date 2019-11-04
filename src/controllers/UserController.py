d21ea6aac7f16b1d72997ad172d029916a2364f3
Joe Farrell <disastrousgamemaster@gmail.com>
2019-11-04 12:30:43 -0500

    Updated with insertion and update queries

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -4,16 +4,19 @@ from src.models.User import Users
 
 class UserController:
 
+    # TODO - turn into env variable
     CONN_STRING="host='localhost' dbname='stocksimulator' user='postgres' password=''"
 
     # Adds a new User to the database
-    def add(self, user: User):
+    def add(self, userName: str, password: str):
         connection = None
         try:
             connection = psycopg.connect(CONN_STRING)
-            
             cursor = connection.cursor()
-            cursor.execute("")
+
+            cursor.execute(f"INSERT INTO UserTable(username) VALUES({userName})")
+            cursor.execute(f"INSERT INTO LoginData(user_id, password) VALUES((SELECT id WHERE username={userName}), SHA256({password}))")
+            
             connection.commit()
             cursor.close()
         except psycopg.DatabaseError as error:
@@ -24,13 +27,12 @@ class UserController:
 
     # Marks existing user as archived
     def archive(self, userId: int):
-        # TODO - implement an archived marker on the User table
         connection = None
         try:
             connection = psycopg.connect(CONN_STRING)
             cursor = connection.cursor()
 
-            cursor.execute("")
+            cursor.execute(f"UPDATE UserData SET archived=TRUE WHERE id={userID}")
 
             connection.commit()
             cursor.close()
