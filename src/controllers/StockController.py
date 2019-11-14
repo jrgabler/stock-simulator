88328c4816f4bc65d5a4774a2aedf37bd8acb457
Joe Farrell <disastrousgamemaster@gmail.com>
2019-11-13 21:42:54 -0500

    Refactored for MySQL dependency

--- src/controllers/StockController.py
+++ src/controllers/StockController.py
@@ -1,21 +1,21 @@
-import psycopg2 as psycopg
+import mysql.connector
 
 class StockController:
 
     # TODO - turn into env variable
-    CONN_STRING = "host='lolcalhost' dbname='stocksimulator' user='postgres' password=''"
+    CONN_STRING = "host='localhost' port=3306 user='user' password=''"
 
     def addWatch(self, stockId: int, userId: int):
         connection = None
         try:
-            connection = psycopg.connect(CONN_STRING)
+            connection = mysql.connector.connect(CONN_STRING)
             cursor = connection.cursor()
 
             cursor.execute(f"INSERT INTO WatchList(stock_id, user_id) VALUES({stockId}, {userId})")
 
             connection.commit()
             cursor.close()
-        except psycopg.DatabaseError as error:
+        except mysql.DatabaseError as error:
             print(error) # TODO - log error
         finally:
             if(connection is not None):
@@ -24,14 +24,14 @@ class StockController:
     def removeWatch(self, stockId: int):
         connection = None
         try:
-            connection = psycopg.connect(CONN_STRING)
+            connection = mysql.connector.connect(CONN_STRING)
             cursor = connection.cursor()
 
             cursor.execute(f"DELETE FROM WatchList WHERE stock_id={stockId}")
 
             connection.commit()
             cursor.close()
-        except psycopg.DatabaseError as error:
+        except mysql.DatabaseError as error:
             print(error) # TODO - log error
         finally:
             if(connection is not None):
--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -1,17 +1,17 @@
-import pyscopg2 as psycopg
+import mysql.connector
 
 from src.models.User import Users
 
 class UserController:
 
     # TODO - turn into env variable
-    CONN_STRING="host='localhost' dbname='stocksimulator' user='postgres' password=''"
+    CONN_STRING = "host='localhost' port=3306 user='user' password=''"
 
     # Adds a new User to the database
     def add(self, userName: str, password: str):
         connection = None
         try:
-            connection = psycopg.connect(CONN_STRING)
+            connection = mysql.connector.connect(CONN_STRING)
             cursor = connection.cursor()
 
             cursor.execute(f"INSERT INTO UserTable(username) VALUES({userName})")
@@ -19,7 +19,7 @@ class UserController:
             
             connection.commit()
             cursor.close()
-        except psycopg.DatabaseError as error:
+        except mysql.DatabaseError as error:
             print(error)
         finally:
             if(connection is not None):
@@ -29,14 +29,14 @@ class UserController:
     def archive(self, userId: int):
         connection = None
         try:
-            connection = psycopg.connect(CONN_STRING)
+            connection = mysql.connector.connect(CONN_STRING)
             cursor = connection.cursor()
 
             cursor.execute(f"UPDATE UserData SET archived=TRUE WHERE id={userID}")
 
             connection.commit()
             cursor.close()
-        except psycopg.DatabaseError as error:
+        except mysql.DatabaseError as error:
             print(error)
         finally:
             if(connection is not None):
