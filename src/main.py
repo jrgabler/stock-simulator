03acbda52a55f43d272cd9580e9f9a63369e7e3f
Julianna Gabler <jrg4017@rit.edu>
2019-11-14 17:03:59 -0500

    update css, macros, forms, etc

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import request
 
 # refactor into config.py file
@@ -11,7 +11,19 @@ STATIC_FOLDER = os.path.abspath(VIEW_DIRECTORY + "/static/")
 
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
 
+# refactor later to appropiate place yo
+# def isLoggedIn(is_log):
+#     # add logic to actually check if logged in or not
+#     return is_log
+#
+# # refactor later to appropiate place yo
+# def redirectIfNotLoggedIn(is_log, redirect_url):
+#     is_logged_in = isLoggedIn(is_log)
+#     if is_logged_in == False:
+#         return redirect(url_for(redirect_url))
+
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template('index.html.j2')
 
@@ -22,6 +34,10 @@ def login():
 @app.route("/register")
 def register():
     return render_template('login/register.html.j2')
+
+@app.route("/reset-password")
+def reset_password():
+    return render_template('login/password-reset.html.j2')
 
 @app.route("/account")
 def account():
