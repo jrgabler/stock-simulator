4a6b8acfe381c7d3a0c766cf14de3223ebdb0bcb
Joe Farrell <disastrousgamemaster@gmail.com>
2019-11-22 12:30:22 -0500

    Authentication in model and login validation in controller

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -1,4 +1,4 @@
-import mysql.connector
+import mysql.connector, binascii, hashlib
 
 from src.models.User import Users
 
@@ -14,8 +14,10 @@ class UserController:
             connection = mysql.connector.connect(CONN_STRING)
             cursor = connection.cursor()
 
+            hashedPassword, salt = hash(password)
+
             cursor.execute(f"INSERT INTO UserTable(username) VALUES({userName})")
-            cursor.execute(f"INSERT INTO LoginData(user_id, password) VALUES((SELECT id WHERE username={userName}), SHA256({password}))")
+            cursor.execute(f"INSERT INTO LoginData(user_id, password, salt) VALUES((SELECT id WHERE username={userName}), {hashedPassword}, {salt})")
             
             connection.commit()
             cursor.close()
@@ -41,3 +43,29 @@ class UserController:
         finally:
             if(connection is not None):
                 connection.close()
+
+    # secure password with sha512
+    def __hash(self, password: str):
+        salt = hashlib.sha512(os.urandom(60)).hexdigest().encode("ascii")
+        hashedValue = hashlib.pbkdf2_hmac("sha512", password.encode("utf-8"), salt, 10000)
+
+        return (salt + binascii.hexlify(hashedValue)), salt
+
+    def validateLogin(self, user: User, userId: str, password: str):
+        connection = None
+        try:
+            connection = mysql.connector.connect(CONN_STRING)
+            cursor = connection.cursor()
+
+            # get the salt and password
+            cursor.execute("SELECT password, salt FROM LoginData WHERE user_id = {userId}")
+            row = cursor.fetchone()
+
+            dbPassword = row[0]
+            salt = row[1]
+
+            if(password == salt + dbPassword):
+                user.authenticate()
+                return True
+
+            return False
--- src/models/User.py
+++ src/models/User.py
@@ -1,11 +1,16 @@
 class User:
-    # Security notes: so we should be encrypting the password on the frontend somehow
-    # and it should never be visible in any way
-    # Apply further encryption on the backend? Or is that overkill?
-    # Is frontend encryption even possible from a client computation standpoint?
+
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
+        self.isAuthenticated = False    # model only
 
     def getUsername(self):
         return self.username
+
+    def isAuthenticated(self):
+        return self.isAuthenticated
+
+    # There's no way this is the correct way to do this
+    def authenticate(self):
+        self.isAuthenticated = True
