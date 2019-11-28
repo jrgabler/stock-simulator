9f4a3879c720544d969642a6b28f925475ef6807
Joe Farrell <disastrousgamemaster@gmail.com>
2019-11-27 22:48:14 -0500

    Modified backend and database structure for functional watchlist endpoints

---
+++ src/api/StockService.py
@@ -0,0 +1,36 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+
+from Controllers import StockController
+from Models.assets import Stock
+from Resources import MarketProvider
+
+parser = reqparse.RequestParser()
+parser.add_argument("stock_symbol", help="", required=False)
+parser.add_argument("user_id", help="", required=False)
+parser.add_argument("stock_id", help="", required=False)
+
+# Arguments: username, stock symbol
+class PurchaseAsset(Resource):
+    @jwt_required
+    def post(self):
+        data = parser.parse_args()
+
+        stock = MarketProvider.getStock(data["stock_symbol"])
+
+        # This is gonna be a hefty boi
+        
+
+class WatchAsset(Resource):
+    @jwt_required
+    def post(self):
+        data = parser.parse_args()
+        stock = MarketProvider.getStock(data["stock_symbol"])
+        
+        return StockController.addWatch(stock, data["user_id"])
+
+class RemoveWatchedAsset(Resource):
+    @jwt_required
+    def post(self):
+        data = parser.parse_args()
+        return StockController.removeWatch(data["stock_id"], data["user_id"])
