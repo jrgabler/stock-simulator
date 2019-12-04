0462fc68a02cfa9b1b6a6dea803dae94405ed2ec
Joe Farrell <disastrousgamemaster@gmail.com>
2019-12-04 17:50:28 -0500

    Code cleanup

--- src/Resources/MarketProvider.py
+++ src/Resources/MarketProvider.py
@@ -6,7 +6,7 @@ from models.Quote import Quote
 class MarketProvider:
 
     # GLOBALS
-    API_TOKEN = "Bearer w8KEVBlgug7OG33LSw2586sZl5gM"  # NEVER EVER COMMIT THIS
+    API_TOKEN = ""  # NEVER EVER COMMIT THIS
     API_URL = "https://sandbox.tradier.com/v1"     # If you change the API you have to update the objects dumped to
 
     HEADERS = {"Authorization": API_TOKEN, "Accept": "application/json"}
