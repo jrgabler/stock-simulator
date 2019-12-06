08c5593d7c87acf97acff6c5f3742d5e1f5ae2e4
Joseph Farrell <jdf4921@rit.edu>
2019-12-05 19:10:08 -0800

    Reverted directory changes to enable existing PRs

--- src/Models/Collections/OwnedAssets.py
+++
@@ -1,5 +0,0 @@
-# Assets currently owned by the user
-class OwnedAssets(AssetMap):
-    def __init__(self, asset: Asset):
-        self.dictId = 1
-        self.asset = {0: asset}
--- src/Models/Collections/SoldAssets.py
+++
@@ -1,5 +0,0 @@
-# Assets historically owned by the user
-class SoldAssets(AssetMap):
-    def __init__(self, asset: Asset):
-        self.dictId = 1
-        self.asset = {0: asset}
