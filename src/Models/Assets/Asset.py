b3ddbdc696850e162a420ebb3dc9a8f320eed842
Joe Farrell <disastrousgamemaster@gmail.com>
2019-12-05 23:09:56 -0500

    Reverted directory changes to enable existing PRs (#36)

--- src/Models/Assets/Asset.py
+++
@@ -1,5 +0,0 @@
-# Parent class to Stock, etc. (coming soon)
-class Asset:
-    def __init__(self, type: str, symbol: str):
-        self.type = type
-        self.symbol = symbol
--- src/Models/Collections/Watchlist.py
+++
@@ -1,5 +0,0 @@
-# Unowned assets being monitored by the user
-class Watchlist(AssetMap):
-    def __init__(self, asset: Asset):
-        self.dictId = 1
-        self.asset = {0: asset}
